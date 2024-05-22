64cf691787f4bcc514c067d1ad06435558d646d1
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-22 15:12:10 +0800

    6.2: Unicafe Revisited, step1

--- src/reducer.js
+++ src/reducer.js
@@ -10,11 +10,11 @@ const counterReducer = (state = initialState, action) => {
     case 'GOOD':
       return { ...state, good: state.good + 1 }
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
-      return state
+      return { ...initialState }
     default: return state
   }
   
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -47,4 +47,38 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state  = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reset to zero', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    const state = {
+      good: 1,
+      ok: 1,
+      bad: 1
+    }
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
